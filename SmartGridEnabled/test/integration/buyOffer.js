488cd9931dd868bcb0b95f7b08668fc8831aac27
Nobert1 <s185031@student.dtu.dk>
2023-11-08 11:41:34 +0100

    moved to another folder, and added some tests based on the supply contract

--- SmartGridEnabled/test/integration/buyOffer.js
+++ SmartGridEnabled/test/integration/buyOffer.js
@@ -3,21 +3,19 @@ const CableCompany = artifacts.require("CableCompany");
 const SmartMeter = artifacts.require("SmartMeter");
 const SupplyContract = artifacts.require("SupplyContract")
 
-contract("Market", (accounts) => {
+contract("All of them", (accounts) => {
 
     let marketInstance;
     let cableCompanyInstance;
     let smartMeterInstance;
-    let supplyContractInstance;
 
     before(async () => {
         cableCompanyInstance = await CableCompany.deployed();
         marketInstance = await Market.new(cableCompanyInstance.address);
         smartMeterInstance = await SmartMeter.deployed();
-        supplyContractInstance = await SupplyContract.deployed();
     });
 
-    it("Should just work lmao", async () => {
+    it("Buy offer", async () => {
         // register key
         await cableCompanyInstance.registerKey(accounts[0], smartMeterInstance.address);
         const isRegisteredTrue = await cableCompanyInstance.isRegisteredKey(accounts[0], smartMeterInstance.address);
@@ -29,14 +27,17 @@ contract("Market", (accounts) => {
         // add charge
         await smartMeterInstance.createLog(0, 5);
         // add offer
-        console.log("accounts[0", accounts[0]);
         await marketInstance.addOffer("1", 1, 1, Date.now() + 1000 * 60 * 60, smartMeterInstance.address, {from: accounts[0]});
         // attempt to buy offer
         await marketInstance.buyOffer("1", {from: accounts[1]});
-
-
-        
-        //assert.equal(isRegistered, true, "Is registered");
+        // check supplycontractaddress
+        const supplyContractAddress = await marketInstance.getLatestSupplyContract();
+        // get info
+        const supplyContractInstance = await SupplyContract.at(supplyContractAddress);
+        const supplyContractInfo = await supplyContractInstance.getInfo();
+        // assert the info of the contract is correct.
+        assert.equal(supplyContractInfo.buyer, accounts[1], "Buyer is not correct");
+        assert.equal(supplyContractInfo.seller, accounts[2], "Seller is not correct");        
     });
 
-})
+});
